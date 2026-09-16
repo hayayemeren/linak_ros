46e65e711693106c80bb64af9fa36a89f0a45663
hayayemeren <hayayemeren@gmail.com>
2026-09-16 11:20:04 +0300

    Changing names to: laundary -> clothwasher washing -> dishwasher

--- launch/servers.launch.py
+++ launch/servers.launch.py
@@ -12,12 +12,13 @@ def generate_launch_description():
         {'name': 'island4', 'mac': 'F8:2B:62:8A:2B:8A', 'offset': 630},
         {'name': 'stove', 'mac': 'F5:42:26:A5:2F:84', 'offset': 610},
         {'name': 'oven', 'mac': 'EA:01:40:0B:4D:0A', 'offset': 750},
-        {'name': 'laundry', 'mac': 'F7:47:02:9A:85:5D', 'offset': 990},
-        {'name': 'washing', 'mac': 'E7:75:89:F7:6E:5E', 'offset': 990},
+        {'name': 'clothwasher', 'mac': 'F7:47:02:9A:85:5D', 'offset': 990},
+        {'name': 'dishwasher', 'mac': 'E7:75:89:F7:6E:5E', 'offset': 990},
         #{'name': 'wall1', 'mac': 'E8:9D:29:FE:52:ED', 'offset': 620},
         #{'name': 'wall2', 'mac': 'E8:9D:29:FE:52:ED', 'offset': 620},
         #{'name': 'wall3', 'mac': 'E8:9D:29:FE:52:ED', 'offset': 620},
         {'name': 'window2', 'mac': 'E4:F7:BB:C7:07:64', 'offset': 620},
+        {'name': 'window3', 'mac': 'D1:B9:0D:B3:19:3A', 'offset': 620}
     ]
 
     node_list = []
